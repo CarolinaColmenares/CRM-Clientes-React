f94743a79a25f4ff8c2483acbfab1f74c9e2c6e5
CarolinaColmenares <yzcarcolmenares@gmail.com>
2022-09-16 16:33:52 -0300

    Eliminando Registros

--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -22,6 +22,25 @@ const Inicio = () => {
     obtenerClientesAPI()
   }, [])
 
+  const handleEliminar = async id => {
+    const confirmar = confirm('¿Deseas Eliminar este Cliente?')
+    if(confirmar){
+      try {
+        const url = `http://localhost:4000/clientes/${id}`
+        const respuesta = await fetch(url,{
+          method: 'DELETE'
+        })
+        await respuesta.json()
+
+        const arrayClientes = clientes.filter( cliente => cliente.id !== id)
+        setClientes(arrayClientes)
+      } catch (error) {
+        console.log(error)
+      }
+
+    }
+  }
+
   return (
     <>
       <h1 className='font-black text-4xl text-blue-900'>Clientes</h1>
@@ -41,6 +60,7 @@ const Inicio = () => {
             <Cliente
               key={cliente.id}
               cliente={cliente}
+              handleEliminar={handleEliminar}
             />
           ))}
         </tbody>

a04174cb52cb7044e314c4ac7c23accb52d0bf3f
CarolinaColmenares <yzcarcolmenares@gmail.com>
2022-09-15 22:14:38 -0300

    Consultar la API para obtener los Clientes

--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 
 const Inicio = () => {
-    return (
-      <div>
-          <h1>Desde Inicio.jsx</h1>
-      </div>
-    )
-  }
-  
-  export default Inicio
+
+  const [clientes, setClientes] = useState([])
+
+  useEffect(() => {
+    const obtenerClientesAPI = async () => {
+
+      try {
+        const url = 'http://localhost:4000/clientes'
+        const respuesta = await fetch(url)
+        const resultado = await respuesta.json()
+
+        console.log(resultado)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    obtenerClientesAPI()
+  }, [])
+
+  return (
+    <div>
+      <h1>Desde Inicio.jsx</h1>
+    </div>
+  )
+}
+
+export default Inicio
